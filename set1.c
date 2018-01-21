279d65f2dd31c16e332cfd586f7d4bfb9fbbc563
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-01-21 15:15:42 +0530

    Create set1.c

---
+++ set1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("The enter number");
+scanf("%d",&a);
+if(a<0)
+{
+printf("The enter value is negative number");
+}
+else
+if(a==0)
+{
+printf("The enter value is zero");
+}
+else
+{
+printf("The enter value is positive number");
+}
+}
