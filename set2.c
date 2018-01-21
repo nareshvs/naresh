ceafd848f5fc89bb59c4d6e588d6f800a53129de
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-01-21 16:39:05 +0530

    Create set2.c

---
+++ set2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the value");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the given value is even");
+}
+else
+{
+printf("The given value is not even");
+}
+}
