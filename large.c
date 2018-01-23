af3da19668c9f63cbd0d9d018e5939f647e46853
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-01-23 16:49:39 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the three numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("\n a is greater than two other numbers");
+}
+else if
+if(b>a&&b>c)
+{
+printf("\n b value is greater");
+}
+else
+{
+printf("\n c value is greater");
+}
+}
