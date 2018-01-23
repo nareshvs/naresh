77a08057cd5886094b550837a3e66e0cb55775dc
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-01-23 16:56:24 +0530

    Create set 4.c

---
+++ set 4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char x;
+printf("Enter any character");
+scanf("%c",&x);
+if((x>='a'&&x<="z"))||((x>='A'&&X<='Z'))
+{
+printf("character is an alphabets");
+}
+else
+{
+printf("character is not alphabets");
+}
+reurn 0;
+}
