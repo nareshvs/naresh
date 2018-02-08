5c24a0de6d8246742c2d18f768fb2188ae0f1995
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-02-08 16:18:21 +0530

    Create set13.c

---
+++ set13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b;
+printf("enterthe value of b");
+scanf("%d",&b);
+if(a<==1)
+{
+printf("number is prime");
+}
+else
+{
+printf("not");
+}
+getch();
+}
