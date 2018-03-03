c9da07c1d54499a44bce675133b071828adf8ebf
nareshvs <35652601+nareshvs@users.noreply.github.com>
2018-03-03 14:22:43 +0530

    Create set25.c

---
+++ set25.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m,n[10],i,j,c,d;
+clrscr();
+printf("Enter the size of array:\n");
+scanf("%d",&m);
+printf("Enter the Elements of array:\n");
+for(i=0;i<a;i++)
+{
+scanf("%d",&m[i]);
+}
+for(i=0;i<m;i++)
+{
+for(j=i+1;j<m;j++)
+{
+if(n[i]>=n[j])
+{
+c=n[i];
+n[i]=n[j];
+n[j]=c;
+}}}
+printf("Sorting order is :\n");
+for(i=0;i<a;i++)
+{
+d=b[i]/2;
+}
+printf("The Median =%d",d);
+getch();
+}
